56a37a9e2037f53fa4273dd78bad49eb29b35ac5
mariomarine <rwbleo@gmail.com>
2020-06-01 18:25:28 -0600

    Use Scale function to define bar height

--- src/d3PriceChart.js
+++ src/d3PriceChart.js
@@ -16,15 +16,19 @@ d3PriceChart.update = (el, state) => {
 	const canvasWidth = 600
 	const scale = .25;
 	let parsed_state = state.map((d) => d["Market Value"]);
-	let svg_elem = d3.select(el)
+	let svg_elem = d3.select(el);
+	var price_scale = d3.scaleLinear()
+	                    //.domain([d3.min(parsed_state), d3.max(parsed_state)])
+											.domain([0, d3.max(parsed_state)])
+											.range([0, canvasHeight]);
 	svg_elem.selectAll("rect")
 		.data(parsed_state).enter()
 		.append("rect")
 		.attr("width", 40)
-		.attr("height", (datapoint) => datapoint * scale)
+		.attr("height", (datapoint) => price_scale(datapoint))
 		.attr("fill", "orange")
 		.attr("x", (datapoint, iteration) => iteration * 45)
-		.attr("y", (datapoint) => canvasHeight - datapoint * scale)
+		.attr("y", (datapoint) => canvasHeight - price_scale(datapoint))
 
 };
 
